51719243c519eeb9d57e8bf133f5bd15c725d3cf
Daniil <dan.developer@born2be.media>
2025-08-13 14:07:58 +0300

    [feature/email-letters]: Registration Email (#31)

--- src/features/auth/ui/registration-success.tsx
+++ src/features/auth/ui/registration-success.tsx
@@ -4,10 +4,13 @@ import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
 import { Button } from '@/shared/ui/components/atoms/button';
+import { useDialogStore } from '@/shared/ui/components/atoms/dialog';
 import { Text } from '@/shared/ui/components/atoms/text';
 
 export const RegistrationSuccess = () => {
   const t = useTranslations('registration.success');
+
+  const { setIsOpen } = useDialogStore();
 
   return (
     <section className="flex flex-col gap-6 text-center">
@@ -16,7 +19,7 @@ export const RegistrationSuccess = () => {
           fallback: 'Registration successful. You can now sign in to your account.',
         })}
       </Text>
-      <Link href="/sign-in">
+      <Link href="/sign-in" onClick={() => setIsOpen(false)}>
         <Button fullWidth>{t('continue', { fallback: 'Continue' })}</Button>
       </Link>
     </section>
