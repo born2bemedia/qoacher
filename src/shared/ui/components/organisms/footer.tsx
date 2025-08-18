3bcc0d07eba26a49e3879094298e3158685fa23c
daniil-b2be <dan.developer@born2be.media>
2025-08-18 17:06:56 +0300

    footer

--- src/shared/ui/components/organisms/footer.tsx
+++ src/shared/ui/components/organisms/footer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
@@ -8,12 +9,73 @@ import { PolicyNavigation } from '@/shared/config/policy-navigation';
 import { Divider } from '@/shared/ui/components/atoms/divider';
 import { Text } from '@/shared/ui/components/atoms/text';
 
+import { FacebookIcon, InstagramIcon, XIcon } from '../../icons/fill/socials';
+
 export const Footer = () => {
   const t = useTranslations('footer');
 
   return (
     <footer className="border-light-gray flex flex-col gap-12 border-t px-[100px] py-12 max-md:px-4">
-      <section className="flex items-center justify-between max-md:flex-col max-md:gap-12">
+      <section className="flex max-md:items-center justify-between max-md:flex-col max-md:gap-12">
+        <section className="flex w-[350px] flex-col gap-6 max-md:w-full max-md:text-center">
+          {/* <Text size="sm" weight={300}>
+            [COMPANY]
+          </Text> */}
+          <Image src="/logo.svg" alt="logo" width={136} height={30} />
+          <Text size="sm" weight={300}>
+            {t('companyDescription', {
+              fallback:
+                'Expert support for personal growth, relationships, and career development in today’s demanding world.',
+            })}
+          </Text>
+          <div className="flex items-center gap-6">
+            <Link
+              href="https://www.facebook.com/qoachercom/ "
+              className="flex items-center justify-center"
+            >
+              <FacebookIcon />
+            </Link>
+            {/* <Link
+            href="#"
+            className="w-[52px] h-[52px] flex items-center justify-center border-1 border-[#808080]"
+          >
+            <YoutubeIcon />
+          </Link> */}
+            <Link
+              href="https://www.instagram.com/qoacher/"
+              className="flex items-center justify-center"
+            >
+              <InstagramIcon />
+            </Link>
+            {/* <Link
+            href="#"
+            className="w-[52px] h-[52px] flex items-center justify-center border-1 border-[#808080]"
+          >
+            <LinkedinIcon />
+          </Link> */}
+            <Link
+              href="https://x.com/SocialsSoc95967 "
+              className="flex items-center justify-center"
+            >
+              <XIcon />
+            </Link>
+          </div>
+          {/* <Text size="sm" weight={300}>
+            {t('registeredAddress', {
+              fallback: 'Registered address:',
+            })}
+          </Text>
+          <Text size="sm" weight={300}>
+            {t('registeredNumber', {
+              fallback: 'Registered number:',
+            })}
+          </Text>
+          <Text size="sm" weight={300}>
+            {t('officeAddress', {
+              fallback: 'Office address:',
+            })}
+          </Text> */}
+        </section>
         <nav className="flex gap-6 max-md:flex-col max-md:items-center max-md:gap-12 max-md:text-center">
           <section className="flex gap-6">
             <div className="flex max-w-[150px] flex-col gap-6 max-md:text-center">
@@ -51,32 +113,6 @@ export const Footer = () => {
               ))}
           </div>
         </nav>
-        <section className="flex w-[350px] flex-col gap-6 max-md:w-full max-md:text-center">
-          <Text size="sm" weight={300}>
-            [COMPANY]
-          </Text>
-          <Text size="sm" weight={300}>
-            {t('companyDescription', {
-              fallback:
-                'Expert support for personal growth, relationships, and career development in today’s demanding world.',
-            })}
-          </Text>
-          <Text size="sm" weight={300}>
-            {t('registeredAddress', {
-              fallback: 'Registered address:',
-            })}
-          </Text>
-          <Text size="sm" weight={300}>
-            {t('registeredNumber', {
-              fallback: 'Registered number:',
-            })}
-          </Text>
-          <Text size="sm" weight={300}>
-            {t('officeAddress', {
-              fallback: 'Office address:',
-            })}
-          </Text>
-        </section>
       </section>
       <Divider />
       <section className="flex items-center justify-between max-md:flex-col max-md:gap-8">
