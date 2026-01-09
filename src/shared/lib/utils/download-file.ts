1a4bcd21e641bd2b4021f80cd4aab6f23af7f4fa
SergWebAcceptance <serg.web@acceptance.ltd>
2026-01-09 12:47:19 +0200

    Add target attribute to download link in downloadFile utility

--- src/shared/lib/utils/download-file.ts
+++ src/shared/lib/utils/download-file.ts
@@ -2,6 +2,7 @@ export const downloadFile = (url: string) => {
   const link = document.createElement('a');
   link.href = url;
   link.download = `invoice-${url.split('/').pop()}`;
+  link.target = '_blank';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
